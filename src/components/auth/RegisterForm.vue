4637c159dfda3e5474b1ae625ffda83b39d273b5
Nikola <nikolaviki@homail.com>
2020-09-12 17:00:07 +0200

    #LAGJ-33 adding register component

---
+++ src/components/auth/RegisterForm.vue
@@ -0,0 +1,61 @@
+<template>
+<div className="sign-up">
+    <h2 className="title">Please register</h2>
+        <form class="sign-up-form">
+          <div class="gruop" >
+            <input
+            class="form-input"
+            name="displayName"
+            type="text"
+            required >
+          </div>
+          <div class="gruop" >
+            <input
+            class="form-input"
+            name="displayName"
+            type="text"
+            required >
+          </div>
+          <div class="gruop" >
+            <input
+            class="form-input"
+            name="displayName"
+            type="text"
+            required >
+          </div>
+          <div class="gruop" >
+            <input
+            class="form-input"
+            name="displayName"
+            type="text"
+            required >
+          </div>
+          <div class="gruop" >
+            <input
+            class="form-input"
+            name="displayName"
+            type="text"
+            required >
+          </div>
+
+      </form>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'RegisterForm',
+}
+</script>
+
+<style scoped>
+.sign-up-form{
+  margin: 50px 0 0 0;
+}
+.form-input{
+  width: 250px;
+  margin: 10px 0 0 0;
+}
+  
+</style>
---
+++ src/views/auth/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+      <RegisterForm/>
+  </div>
+</template>
+
+<script>
+import RegisterForm from '@/components/auth/RegisterForm.vue'
+
+export default {
+  name: 'Register',
+  components: {
+    RegisterForm
+  }
+}
+</script>
