ad0675d5c55809fdb90ffa48d33899643f17019d
Nikola <nikolaviki@homail.com>
2020-09-14 18:10:32 +0200

    #LAGJ-36 add delete component

---
+++ src/components/todos/DeleteComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="remove-item" @click="handleDelete" >
+        &times;
+    </div>
+</template>
+<script>
+export default {
+    name: 'DeleteComponent',
+    props:{
+        todo:{
+            type: Object,
+            reqired: true
+        }
+    },
+    methods: {
+        handleDelete(){
+            console.log(this.todo.id)
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+.remove-item{
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover{
+        color: black;
+    }
+}
+
+    
+</style>
--- src/views/todos/Todos.vue
+++ src/views/todos/Todos.vue
@@ -2,18 +2,21 @@
     <div class="item-wraper">
         <div class="todo-item" v-for="todo in getAllTodosFromState" :key="todo.id">
             <SingleTodo :todo="todo"/>
+            <DeleteCompoennt :todo="todo"/>
         </div>
     </div>
 </template>
 
 <script>
 import SingleTodo from '../../components/todos/SIngleTodo'
+import DeleteCompoennt from '../../components/todos/DeleteComponent'
 import { mapActions, mapGetters } from "vuex";
 
 export default {
     name: 'Todos',
     components:{
         SingleTodo,
+        DeleteCompoennt
     },
     methods:{
         ...mapActions(['getTodos'])
