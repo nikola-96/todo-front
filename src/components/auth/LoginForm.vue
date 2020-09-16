c1c087c5cf7f7baf24f9dbba907787bead7f2088
Nikola <nikolaviki@homail.com>
2020-09-16 12:10:17 +0200

    added btn redirection logic

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,64 +1,72 @@
 <template>
-<div className="sign-up">
-    <h2 className="title">Please login</h2>
-        <form class="sign-up-form" @submit.prevent="handleSubmit">
-          <div class="gruop" >
-            <input
-            class="form-input"
-            id="email"
-            name="email"
-            type="email"
-            v-model="user.email"
-            required
-            placeholder="Enter your email:">
-          </div>
-          <div class="gruop" >
-            <input
-            class="form-input"
-            id="password"
-            name="password"
-            type="password"
-            v-model="user.password"
-            required
-            placeholder="Enter your password:" >
-          </div>
-        <button class="btn-submit btn btn-secondary" type="submit">Submit</button>
-      </form>
-</div>
+  <div class="sign-up">
+    <h2 class="title">Please login</h2>
+    <form class="sign-up-form" @submit.prevent="handleSubmit">
+      <div class="gruop">
+        <input
+          class="form-input"
+          id="email"
+          name="email"
+          type="email"
+          v-model="user.email"
+          required
+          placeholder="Enter your email:"
+        />
+      </div>
+      <div class="gruop">
+        <input
+          class="form-input"
+          id="password"
+          name="password"
+          type="password"
+          v-model="user.password"
+          required
+          placeholder="Enter your password:"
+        />
+      </div>
+      <button class="btn-submit btn btn-secondary" type="submit">Submit</button>
+      <ButtonComponent class="btn-register" />
+    </form>
+  </div>
 </template>
 
 <script>
-
+import ButtonComponent from "../todos/ButtonComponent";
 export default {
-  name: 'RegisterForm',
-  props:{
-      user:{
-          type: Object,
-          required: true
-      },
-      handleLogin:{
-          type: Function,
-          required: true
-      }
+  name: "RegisterForm",
+  components: {
+    ButtonComponent,
   },
-    methods: {
-        handleSubmit(){
-            this.handleLogin(this.user)
-        }
-    }
-}
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+    handleLogin: {
+      type: Function,
+      required: true,
+    },
+  },
+  methods: {
+    handleSubmit() {
+      this.handleLogin(this.user);
+    },
+  },
+};
 </script>
 
 <style scoped>
-.sign-up-form{
+.sign-up-form {
   margin: 50px 0 0 0;
 }
-.form-input{
+.form-input {
   width: 300px;
   margin: 10px 0 0 0;
 }
-.btn-submit{
+.btn-submit {
   margin: 15px 0 0 0;
 }
-  
+.btn-register {
+  margin: 15px 0 0 150px;
+}
 </style>
---
+++ src/components/todos/ButtonComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    class="button btn btn-secondary"
+    @click="()=>this.$router.push({path: this.getRouteName()})"
+  >{{getRouteName()}}</button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+  methods: {
+    getRouteName() {
+      if (this.$router.currentRoute.name === "Login") {
+        return "Register";
+      } else if (this.$router.currentRoute.name === "Register") {
+        return "Login";
+      }
+    },
+  },
+};
+</script>
